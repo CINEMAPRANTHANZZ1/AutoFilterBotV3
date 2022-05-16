c82e51168b1e16e86c5dbdfe0e903ed5495eebcd
DHANANJAY P.H <102791126+DHANANJAY4532@users.noreply.github.com>
2022-05-16 16:42:21 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -111,7 +111,7 @@ async def start(bot, message):
     else:
         await message.reply_photo(
             photo="https://telegra.ph/file/d9550da87ea5cd26f5a63.jpg",
-            caption="Enter Your Text Here https://t.me/CINEMAPRANTHANZZ1",
+            caption="default_start_msg https://t.me/CINEMAPRANTHANZZ1",
             START_MSG,
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
